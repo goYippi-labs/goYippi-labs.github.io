0ce209a1c0d9399fd589dfa67c6813cbb78b4a3c
Christian Roth <christian@christismacbook.Speedport_W723_V_Typ_A_1_01_016>
2016-08-05 15:30:58 +0200

    Added goYippi labs Blog

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,5 +1,3 @@
-// Based on http://yonaba.github.io/2012/08/14/List-your-GitHub-projects-using-JavaScript-and-jQuery.md.html
-
 var $=jQuery;
 
 $.githubUser = function(username, gitpart, callback) {
@@ -7,6 +5,8 @@ $.githubUser = function(username, gitpart, callback) {
 }
 
 $.fn.loadGitInfos = function(username, gitpart) {
+    // Credit: http://yonaba.github.io/2012/08/14/List-your-GitHub-projects-using-JavaScript-and-jQuery.md.html
+
     this.html("<span>Querying GitHub for " + username + "'s " + gitpart + "...</span>");
 
     var target = this;
@@ -38,9 +38,37 @@ $.fn.loadGitInfos = function(username, gitpart) {
     }
 };
 
+$.fn.loadBlog = function(url) {
+    //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+    this.html("<span>Querying goYippi Blog for " + url + "...</span>");
+
+    var target = this;
+
+    $.ajax(url, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        dataType:"xml",
+        success:function(data) {
+            var i = 0;
+            var list = $('<dl/>');
+            target.empty().append(list);
+            $(data).find("item").each(function () {
+                i++;
+                if (i <= 2) {
+                    list.append('<dt><a href="'+ $(this).find("link").text() +'">' + $(this).find("title").text() + '</a></dt>');
+                    list.append('<dd>' + $(this).find("description").text() + '</dd>');
+                }
+            });
+        }
+    });
+}
+
 $(document).ready(function($) {
 	$('html').removeClass('no-js').addClass('js-active');
 
     $("#github-repos").loadGitInfos('goYippi-labs', 'repos');
     $("#github-gists").loadGitInfos('Christian-Roth', 'gists');
+    $("#goyippi-blog").loadBlog('http://www.goyippi.net/tag/goyippi-labs/feed/');
 });
